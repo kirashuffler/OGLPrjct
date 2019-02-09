4e7d1000ba15f099f33da8517409eceeb87c2f6c
kirashuffler <kirashuffler@gmail.com>
2019-02-09 14:57:17 +0300

    Add files via upload
    Проект был реализован в среде Qt.

---
+++ fshader.fsh
@@ -0,0 +1,27 @@
+uniform sampler2D u_texture;
+uniform highp vec4 u_lightPosition;
+uniform highp float u_lightPower;
+varying highp vec2 v_texcoord;
+varying highp vec4 v_position;
+varying highp vec3 v_normal;
+
+void main(void)
+{   vec4 resultColor = vec4(0.0, 0.0, 0.0, 0.0);
+    vec4 eyePosition = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 diffMatColor = texture2D(u_texture, v_texcoord);
+    vec3 eyeVect = normalize(v_position.xyz - eyePosition.xyz);
+    vec3 lightVect = normalize(v_position.xyz - u_lightPosition.xyz);
+    vec3 reflectLight = normalize(reflect(lightVect, v_normal));
+    float specularFactor = 200.0;
+    float ambientFactor = 0.2;
+    float len = length(v_position.xyz - eyePosition.xyz);
+    vec4 diffColor = diffMatColor * u_lightPower * max(0.0, dot(v_normal, -lightVect) / (1.0 + 0.25 * pow(len, 2.0)));
+    resultColor += diffColor;
+    vec4 ambientColor = ambientFactor * diffMatColor;
+    resultColor += ambientColor;
+    vec4 specularColor = vec4(1.0, 1.0, 1.0, 1.0) * u_lightPower * pow(max(0.0, dot(reflectLight, -eyeVect)), specularFactor) / (1.0 + 0.25 * pow(len, 2.0));
+    resultColor += specularColor;
+
+    gl_FragColor = resultColor;
+
+}
---
+++ widget.cpp
@@ -0,0 +1,150 @@
+#include "widget.h"
+
+#include <QMouseEvent>
+
+Widget::Widget(QWidget *parent)
+    : QOpenGLWidget(parent), m_texture(0), m_indexBuffer(QOpenGLBuffer::IndexBuffer)
+{
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::initializeGL()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    initShaders();
+    initTetr();
+
+}
+
+void Widget::resizeGL(int w, int h)
+{
+    float aspect = w / (float)h;
+    m_projectionMatrix.setToIdentity();
+    m_projectionMatrix.perspective(45, aspect, 0.1f, 10.0f);
+}
+
+void Widget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    QMatrix4x4 viewMatrix;
+    QMatrix4x4 modelMatrix;
+    modelMatrix.setToIdentity();
+    viewMatrix.setToIdentity();
+    viewMatrix.translate(0.0, 0.0, -5.0);
+    viewMatrix.rotate(m_rotation);
+    m_texture->bind(0);
+    m_program.bind();
+    m_program.setUniformValue("u_projectionMatrix", m_projectionMatrix);
+    m_program.setUniformValue("u_viewMatrix", viewMatrix);
+    m_program.setUniformValue("u_modelMatrix", modelMatrix);
+    m_program.setUniformValue("u_texture", 0);
+    m_program.setUniformValue("u_lightPosition", QVector4D(0.0, 0.0, 0.0, 1.0));
+    m_program.setUniformValue("u_lightPower", 6.0f);
+    int offset = 0;
+    m_arrayBuffer.bind();
+    int vertLoc = m_program.attributeLocation("a_position");
+    m_program.enableAttributeArray(vertLoc);
+    m_program.setAttributeBuffer(vertLoc, GL_FLOAT, offset, 3, sizeof(VertexData));
+    offset += sizeof(QVector3D);
+
+    int texLoc = m_program.attributeLocation("a_texcoord");
+    m_program.enableAttributeArray(texLoc);
+    m_program.setAttributeBuffer(texLoc, GL_FLOAT, offset, 2, sizeof(VertexData));
+
+    offset += sizeof(QVector2D);
+
+    int normLoc = m_program.attributeLocation("a_normal");
+    m_program.enableAttributeArray(normLoc);
+    m_program.setAttributeBuffer(normLoc, GL_FLOAT, offset, 3, sizeof(VertexData));
+
+    m_indexBuffer.bind();
+    glDrawElements(GL_TRIANGLES, m_indexBuffer.size(), GL_UNSIGNED_INT, 0);
+}
+
+void Widget::initShaders()
+{
+    if(!m_program.addShaderFromSourceFile(QOpenGLShader::Vertex, ":/vshader.vsh"))
+        close();
+    if(!m_program.addShaderFromSourceFile(QOpenGLShader::Fragment, ":/fshader.fsh"))
+        close();
+    if(!m_program.link())
+        close();
+}
+
+void Widget::initTetr()
+{
+    QVector<VertexData> vertexes;
+
+    vertexes.append(VertexData(QVector3D(0.0f, 1.0f, 0.0f), QVector2D(0.5, 1.0), QVector3D(2.25, 1.0, 1.5)));
+    vertexes.append(VertexData(QVector3D(0.0f, -0.5f, 1.0f), QVector2D(0.0, 0.0), QVector3D(2.25, 1.0, 1.5)));
+    vertexes.append(VertexData(QVector3D(1.0f, -0.5f, -0.5f), QVector2D(1.0, 0.0), QVector3D(2.25, 1.0, 1.5)));
+
+    vertexes.append(VertexData(QVector3D(0.0f, 1.0f, 0.0f), QVector2D(0.5, 1.0), QVector3D(-2.25, 1.0, 1.5)));
+    vertexes.append(VertexData(QVector3D(-1.0f, -0.5f, -0.5f), QVector2D(0.0, 0.0), QVector3D(-2.25, 1.0, 1.5)));
+    vertexes.append(VertexData(QVector3D(0.0f, -0.5f, 1.0f), QVector2D(1.0, 0.0), QVector3D(-2.25, 1.0, 1.5)));
+
+    vertexes.append(VertexData(QVector3D(0.0f, 1.0f, 0.0f), QVector2D(0.5, 1.0), QVector3D(0.0, -1.0, -3.0)));
+    vertexes.append(VertexData(QVector3D(-1.0f, -0.5f, -0.5f), QVector2D(0.0, 0.0), QVector3D(0.0, -1.0, -3.0)));
+    vertexes.append(VertexData(QVector3D(1.0f, -0.5f, -0.5f), QVector2D(1.0, 0.0), QVector3D(0.0, -1.0, -3.0)));
+
+    vertexes.append(VertexData(QVector3D(-1.0f, -0.5f, -0.5f), QVector2D(0.0, 0.0), QVector3D(0.0, -1.0, 0.0)));
+    vertexes.append(VertexData(QVector3D(0.0f, -0.5f, 1.0f), QVector2D(0.5, 1), QVector3D(0.0, -1.0, 0.0)));
+    vertexes.append(VertexData(QVector3D(1.0f, -0.5f, -0.5f), QVector2D(1.0, 0.0), QVector3D(0.0, -1.0, 0.0)));
+
+    QVector<GLuint> indexes;
+    indexes.append(2);
+    indexes.append(0);
+    indexes.append(1);
+
+    indexes.append(5);
+    indexes.append(3);
+    indexes.append(4);
+
+    indexes.append(8);
+    indexes.append(7);
+    indexes.append(6);
+
+    indexes.append(11);
+    indexes.append(10);
+    indexes.append(9);
+
+    m_arrayBuffer.create();
+    m_arrayBuffer.bind();
+    m_arrayBuffer.allocate(vertexes.constData(), vertexes.size() * sizeof(VertexData));
+    m_arrayBuffer.release();
+
+    m_indexBuffer.create();
+    m_indexBuffer.bind();
+    m_indexBuffer.allocate(indexes.constData(), indexes.size() * sizeof(GLuint));
+    m_indexBuffer.release();
+
+    m_texture = new QOpenGLTexture(QImage(":/iron.jpg").mirrored());
+    m_texture->setMinificationFilter(QOpenGLTexture::Nearest);
+    m_texture->setMagnificationFilter(QOpenGLTexture::Linear);
+    m_texture->setWrapMode(QOpenGLTexture::Repeat);
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->buttons() == Qt::LeftButton)
+    m_mousePosition = QVector2D(event->localPos());
+    event->accept();
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(event->buttons() != Qt::LeftButton) return;
+    QVector2D diff = QVector2D(event->localPos()) - m_mousePosition;
+    m_mousePosition = QVector2D(event->localPos());
+    float angle = diff.length() / 2.0;
+    QVector3D axis = QVector3D(diff.y(), diff.x(), 0.0);
+    m_rotation = QQuaternion::fromAxisAndAngle(axis, angle) * m_rotation;
+    update();
+}
